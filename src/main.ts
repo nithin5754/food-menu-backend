55bec2ba2b60aa66f4f311cf526605a228a1e0b7
Nithin Joji0756 <nithinjoji0756@gmail.com>
2025-03-13 01:25:47 +0530

    update error-3

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin:[ "http://localhost:5173","https://food-menu-umber.vercel.app/api"],
+    origin:[ "http://localhost:5173","https://food-menu-umber.vercel.app"],
     credentials: true,
   })
 );
